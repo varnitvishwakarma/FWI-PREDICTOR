5802cd9a8f4dcaf16118f897b6a65f3b27c4c573
varnitvishwakarma <varnitusar25@gmail.com>
2023-08-28 14:57:07 +0530

    for deployment

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd 
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
+application = Flask(__name__)
+app=application
 
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scalers.pkl','rb'))
